c5c749e95d611a78ab44ec278c05a2fb81797e7f
alfredo-oss <alfredo.derodt@gmail.com>
2024-04-23 20:02:49 -0400

    Opciones Mixtas

--- entrega_ruuf.py
+++ entrega_ruuf.py
@@ -1,8 +1,13 @@
-def ruuf(ruuf_x, ruuf_y ,panel_x, panel_y):
-    if ruuf_x >=1 and ruuf_y >=1 and panel_x >=1 and panel_y >=1:
-        ocupacion_vertical = (ruuf_x // panel_y) * (ruuf_y // panel_x)
-        ocupacion_horizontal = (ruuf_x // panel_x) * (ruuf_y // panel_y)
-        return max(ocupacion_horizontal, ocupacion_vertical)
+def ruuf(ruuf_x, ruuf_y, panel_x, panel_y):
+    if ruuf_x >= 1 and ruuf_y >= 1 and panel_x >= 1 and panel_y >= 1:
+        if panel_x > ruuf_x and panel_y > ruuf_y:
+            print('Caben 0')
+        else:
+            ocupacion_vertical = (ruuf_x // panel_x) * (ruuf_y // panel_y)
+            ocupacion_horizontal = (ruuf_x // panel_y) * (ruuf_y // panel_x)
+            ocupacion_mixta_horizontal = (ruuf_x // panel_x) * (ruuf_y // panel_y) + (ruuf_y % panel_y) // panel_x
+            ocupacion_mixta_vertical = (ruuf_x // panel_y) * (ruuf_y // panel_x) + (ruuf_x % panel_y) // panel_x
+            print(f'Caben {max(ocupacion_horizontal, ocupacion_vertical, ocupacion_mixta_horizontal, ocupacion_mixta_vertical)}')
     else:
         print("Las dimensiones deben ser mayores o iguales a uno.")
 
@@ -10,5 +15,4 @@ x = 3
 y = 5
 a = 1
 b = 2
-
-print(ruuf(x,y,a,b))
+ruuf(x, y, a, b)
